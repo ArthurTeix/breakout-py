d52145fe1e27a47634cae3e11b8bc040f4cd8a63
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-15 17:26:11 -0300

    feat: create title

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 
-screen_size = (800, 800)
-
+screen_size = (800, 800) 
 screen = pygame.display.set_mode(screen_size) # create screen
+
+pygame.display.set_caption("Breakout") # title
