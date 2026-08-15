0b6118f86cfaa976f301e362acbe7128ddb7f802
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-15 17:38:37 -0300

    feat: size ball and player

--- game.py
+++ game.py
@@ -8,7 +8,10 @@ screen = pygame.display.set_mode(screen_size) # create screen
 pygame.display.set_caption("Breakout") # title
 
 ball_size = 15
+ball = pygame.Rect(100, 500, ball_size, ball_size) # init position ball
+
 width_player = 100
+player = pygame.Rect(0, 750, width_player, 15)
 
 # Number of blocks
 number_blocks_line = 8
